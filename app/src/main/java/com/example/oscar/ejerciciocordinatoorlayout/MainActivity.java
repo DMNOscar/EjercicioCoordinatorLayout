a7ce12a5ffee00800ed0cb28f93c8ca9636bd0c6
DMNoscar <oscardanielmn@gmail.com>
2018-06-21 10:49:22 -0500

    Barra Inferior fija

--- app/src/main/java/com/example/oscar/ejerciciocordinatoorlayout/MainActivity.java
+++ app/src/main/java/com/example/oscar/ejerciciocordinatoorlayout/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.oscar.ejerciciocordinatoorlayout;
 
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +10,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import static com.example.oscar.ejerciciocordinatoorlayout.R.menu.menu_compra_tool_bar;
 
 public class MainActivity extends AppCompatActivity {
-
+    int sheetBehavior;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,17 +39,44 @@ public class MainActivity extends AppCompatActivity {
      Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(mActionBarToolbar);
         getSupportActionBar().setTitle("¿Qué es Lorem Ipsum?");
-        getSupportActionBar().setSubtitle("About");
+        //getSupportActionBar().setSubtitle("About");
 
-
+        sheetBehavior = BottomSheetBehavior.PEEK_HEIGHT_AUTO;
         Button mensaje = (Button) findViewById(R.id.Mensaje);
 
         mensaje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Esta es la mugre que se pone arriba del boton", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Esta es la mugre que se pone arriba del boton", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
+        });
+
+        final EditText textoComentario= (EditText) findViewById(R.id.edtComentario);
+        Button opinion = (Button) findViewById(R.id.btnExtraerTexto);
+        opinion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Tu opinion es: "+ textoComentario.getText(), Toast.LENGTH_SHORT).show();
+                textoComentario.setText("");
+            }
+        });
+
+
+        FloatingActionButton fbVentana= (FloatingActionButton) findViewById(R.id.fbMostrarVentanaInferior);
+
+        fbVentana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  if(sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED){
+
+            //        sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+//
+             //   } else {
+             //       sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+
+                }
+          //  }
         });
     }
 }
